7b422ce3351ba389c1aa92d1ed612b00b8fddff8
HanHyunSoo <gustn4563@gmail.com>
2023-03-21 20:00:45 +0900

    Initial Project

---
+++ src/main/java/com/example/prtest/PrTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.prtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prtest/PrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
