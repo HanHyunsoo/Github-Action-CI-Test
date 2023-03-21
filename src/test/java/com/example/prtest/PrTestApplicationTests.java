a1948fcaa420c42039e6c99bdc53b7aeac116ebf
HanHyunSoo <gustn4563@gmail.com>
2023-03-21 20:40:28 +0900

    test: Add Test

--- src/test/java/com/example/prtest/PrTestApplicationTests.java
+++ src/test/java/com/example/prtest/PrTestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.prtest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,9 @@ class PrTestApplicationTests {
     void contextLoads() {
     }
 
+    @DisplayName("테스트")
+    @Test
+    void test() {
+        assert 1 == 2;
+    }
 }
