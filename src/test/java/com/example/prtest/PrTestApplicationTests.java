9d9f459f740e9075e1e94f5db6a8df6d7afab9e5
HanHyunSoo <gustn4563@gmail.com>
2023-03-21 20:05:04 +0900

    test: Add Fail Case

--- src/test/java/com/example/prtest/PrTestApplicationTests.java
+++ src/test/java/com/example/prtest/PrTestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.prtest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,9 @@ class PrTestApplicationTests {
     void contextLoads() {
     }
 
+    @DisplayName("실패 케이스")
+    @Test
+    void failCase() {
+        assert 1 == 2;
+    }
 }
